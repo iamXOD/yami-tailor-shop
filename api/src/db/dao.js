4796c3dd2da5680bd34ed4b5aab827559dcd78ef
iamXOD <himunoz97@gmail.com>
2021-01-12 14:36:52 -0500

    ♻ refactor(api): refactor database access object
    Create functions to run queries to the database and replace calls to
    db.run() and db.get().

--- api/src/db/dao.js
+++ api/src/db/dao.js
@@ -15,54 +15,52 @@ all = function(collection) {
 };
 
 get = function(collection, id) {
+    const sql = `SELECT * FROM ${collection} WHERE ${collection}_id = ?`;
+    return find(sql, [id]).then(row => {
+        if (!row) { throw new Error(`${collection} with id ${id} not found`) }
+        return row;
+    })
+}
+
+insert = function(collection, newObject) {
+    const props = Object.keys(newObject);
+    const propsString = props.join(", ");
+    const valuesString = props.length == 1 ? "?" : "?,".repeat(props.length - 1) + "?";
+    const sql = `INSERT INTO ${collection} (${propsString})VALUES (${valuesString})`;
+    const values = props.map(prop => newObject[prop])
+    return query(sql, [...values]);
+}
+
+update = function(collection, newObject) {
+    const idName = collection + "_id";
+    const id = newObject[idName];
+    const props = Object.keys(newObject).filter(value => value != idName);
+    const query = props.map(value => `${value} = ?`).join(", ");
+    const values = props.map(value => newObject[value]);
+    const sql = `UPDATE ${collection} SET ${query} WHERE ${idName} = ?`;
+    return query(sql, [...values, id]);
+
+}
+
+remove = function(collection, id) {
+    const sql = `DELETE FROM ${collection} WHERE ${collection}_id = ?`;
+    return query(sql, [id]);
+}
+
+find = function(query, params) {
     return dbWrapper((resolve, reject, db) => {
-        const sql = `SELECT * FROM ${collection} WHERE ${collection}_id = ?`;
-        db.get(sql, [id], (err, row) => {
-            if (err) { reject(err) }
-            if (!row) { reject(`${collection} with id ${id} not found`) }
+        db.get(query, params, (err, row) => {
+            if (err) { reject(err) };
             resolve(row);
         })
     })
 }
 
-insert = function(collection, newObject) {
+query = function(query, params) {
     return dbWrapper((resolve, reject, db) => {
-        const props = Object.keys(newObject);
-        const propsString = props.join(", ");
-        const valuesString = props.length == 1 ? "?" : "?,".repeat(props.length - 1) + "?";
-        const sql = `INSERT INTO ${collection} (${propsString})VALUES (${valuesString})`;
-        const values = props.map(prop => newObject[prop])
-
-        db.run(sql, [...values], function(err) {
-            if (err) { reject(err) };
-            resolve(this.lastID);
-        })
-    });
-}
-
-update = function(collection, newObject) {
-    return dbWrapper((resolve, reject, db) => {
-        const idName = collection + "_id";
-        const id = newObject[idName];
-        const props = Object.keys(newObject)
-            .filter(value => value != idName);
-        const query = props.map(value => `${value} = ?`)
-            .join(", ");
-        const values = props.map(value => newObject[value]);
-        const sql = `UPDATE ${collection} SET ${query} WHERE ${idName} = ?`;
-        db.run(sql, [...values, id], function(err) {
+        db.run(query, params, function(err) {
             if (err) { reject(err) }
-            resolve(this.changes);
-        })
-    })
-}
-
-remove = function(collection, id) {
-    return dbWrapper((resolve, reject, db) => {
-        const sql = `DELETE FROM ${collection} WHERE ${collection}_id = ?`;
-        db.run(sql, [id], function(err) {
-            if (err) { reject(err) }
-            resolve(this.changes);
+            resolve({ lastID: this.lastID, changes: this.changes });
         })
     })
 }
@@ -73,5 +71,7 @@ module.exports = {
     get,
     insert,
     update,
-    remove
+    remove,
+    find,
+    query
 }
