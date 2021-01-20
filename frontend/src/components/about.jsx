c0bb9633158994271437ca728c40eb1983a6491b
iamXOD <himunoz97@gmail.com>
2021-01-20 13:20:33 -0500

    ✨ feat(frontend): add sample about page
    Temporally page about the business

---
+++ frontend/src/components/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = (props) => {
+    return <section>
+        <h1>About Us</h1>
+        <p>We are a tailor shop whose mission
+        is to satisfy the costumers by
+        making clothes made to your
+        measures and taste</p>
+    </section>
+}
+
+export default About;
