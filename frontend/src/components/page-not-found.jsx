055a8a172ec8e0227d2a4ad9cde4f3fa2def3176
iamXOD <himunoz97@gmail.com>
2021-01-20 13:21:02 -0500

    ✨ feat(frontend): add not found page

---
+++ frontend/src/components/page-not-found.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageNotFound = () => {
+    return <section>
+        <h1>Page Not Found</h1>
+        <p>The page you are looking for does not exists.</p>
+    </section>
+}
+export default PageNotFound;
